e706663f7f8873ca9427865eb832b691bee33d65
unknown <scottmortonashton@gmail.com>
2014-06-11 18:12:19 -0600

    Substantial changes to create the fastRead method

---
+++ test/tests.js
@@ -0,0 +1,25 @@
+var expect = require('expect.js'),
+    bowerResolve = require('..'),
+    fs = require('fs'),
+    path = require('path');
+
+describe('bower-resolve', function() {
+
+
+  it('should be able to resolve to a bower component', function(done) {
+    this.timeout(5000);
+    bowerResolve.init(function () {
+      expect(bowerResolve('js-base64')).to.equal(path.join(process.cwd(), "vendor", "js-base64", "base64.js"));
+      done();
+    });
+  });
+
+  it('should fast resolve a bower component', function(done) {
+
+      this.timeout(5000);
+      bowerResolve.fastRead("js-base64", {basedir: process.cwd() + "/test"}, function (pathName) {
+          expect(pathName).to.equal(path.join(process.cwd(), "vendor", "js-base64", "base64.js"));
+          done();
+      });
+  });
+});
